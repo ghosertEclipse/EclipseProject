71c70183524ceab56805cf10235e9b2e8d880ade
Zhang Jiawei <jiawzhang@ubuntu.ubuntu-domain>
2010-05-03 10:34:54 +0800

    first commit

---
+++ workspace/TestJavaProject/src/TestJava.java
@@ -0,0 +1,14 @@
+
+public class TestJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("0");
+
+	}
+
+}
---
+++ workspace/TestPythonProject/src/TestPython.py
@@ -0,0 +1,11 @@
+'''
+Created on May 3, 2010
+
+@author: jiawzhang
+'''
+
+if __name__ == '__main__':
+    
+    import re
+    
+    print re.search(r'^jiawei', 'jiaweizhang')
