0c6fc0f9be5e82edd151f0071f0148589baf567b
jiawzhang <ghosert@gmail.com>
2011-02-11 22:31:53 +0800

    Update

--- python-workspace/anypai/src/test_webkit.py
+++ python-workspace/anypai/src/test_webkit.py
@@ -6,34 +6,44 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from PyQt4.QtWebKit import *
 
-def load_finished(status):
-    element = frame.findFirstElement("input.lst")
-    frame.evaluateJavaScript("q = document.getElementsByName('q')[0]; q.value = 'jiawei'; b = document.getElementsByName('btnG')[0]; b.click();")
-    # element.setAttribute("value", "jiawzhang")
-    # print element.attribute("class")
+class MainContainer(QMainWindow):
+    def __init__(self, parent = None):
+        QMainWindow.__init__(self, parent)
+        
+        # Enable plugins here will make activex turn on, it's important to taobao security activex control
+        # jiawzhang TODO: may be disable QWebSettings.AutoLoadImages for speeding up page loading later ?
+        QWebSettings.globalSettings().setAttribute(QWebSettings.PluginsEnabled, True)
+        
+        self.view = QWebView(self)
+        self.setCentralWidget(self.view)
+        
+        self.page = self.view.page()
+        self.frame = self.page.currentFrame()
+        
+        # view.load(QUrl("http://www.google.com"))
+        # view.load(QUrl("http://item.taobao.com/item.htm?id=9190349629"))
+        self.view.load(QUrl("http://www.taobao.com"))
+        print 'jiawei'
+        
+        # view.connect(view, SIGNAL('loadFinished(bool)'), load_finished)
+        # view.connect(view, SIGNAL('loadProgress(int)'), load_progress)
+    
+    def createWindow(self, webWindowType):
+        print 'new window is creating'
+        return QWebView()
+        
+    def load_finished(self, status):
+        # element = self.frame.findFirstElement("input.lst")
+        self.frame.evaluateJavaScript("q = document.getElementsByName('q')[0]; q.value = 'jiawei'; b = document.getElementsByName('btnG')[0]; b.click();")
+        # element.setAttribute("value", "jiawzhang")
+        # print element.attribute("class")
+    
+    def load_progress(self, progress):
+        print progress
 
-def load_progress(progress):
-    print progress
-
-app = QApplication(sys.argv)
-
-# Enable plugins here will make activex turn on, it's important to taobao security activex control
-# jiawzhang TODO: may be disable QWebSettings.AutoLoadImages for speeding up page loading later ?
-QWebSettings.globalSettings().setAttribute(QWebSettings.PluginsEnabled, True)
-
-view = QWebView()
-page = view.page()
-frame = page.currentFrame()
-
-# view.load(QUrl("http://www.google.com"))
-# view.load(QUrl("http://item.taobao.com/item.htm?id=9190349629"))
-view.load(QUrl("http://www.taobao.com"))
-
-# view.connect(view, SIGNAL('loadFinished(bool)'), load_finished)
-# view.connect(view, SIGNAL('loadProgress(int)'), load_progress)
-
-
-view.show()
-
-app.exec_()
-
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = MainContainer()
+    main.show()
+    app.exec_()
+    
